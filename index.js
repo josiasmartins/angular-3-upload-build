c9e3f064b203470e85fda1a685f2a0d9a4dde417
Caitano <josiasmartinscaitano@gmail.com>
2022-02-06 10:20:19 -0300

    adicionado testes unitario

---
+++ index.js
@@ -0,0 +1,7 @@
+texto  = /\xhhh/;
+const regex = new RegExp(texto);
+
+var myRe = /oi/g;
+console.log(myRe.exec('oi'))
+
+console.log(regex)
---
+++ src/app/modules/01-fundaments/compute.spec.ts
@@ -0,0 +1,13 @@
+import { compute } from './compute';
+
+fdescribe('compute', function() {
+    it('should return 0 if input is negative', () => {
+      const result = compute(-1);
+      expect(result).toBe(0);
+    });
+
+    it('should increment the input if it is positive', () => {
+      const result = compute(1);
+      expect(result).toBe(2);
+    })
+})
