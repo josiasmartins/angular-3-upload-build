31e14659f40e62ae2c7d11cba695698122080689
Caitano <josiasmartinscaitano@gmail.com>
2022-09-06 18:07:22 -0300

    feat: O formulário upload de fotos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      FormsModule,
+      VMessageModule
+    ]
 })
 export class PhotoFormModule { }
