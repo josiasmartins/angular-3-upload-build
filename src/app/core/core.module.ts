f1cc7bf26a9466597a26b044f60889a0edabee20
Caitano <josiasmartinscaitano@gmail.com>
2022-09-06 17:43:06 -0300

    feat: initial project

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
+
+@NgModule({
+    declarations: [HeaderComponent],
+    exports: [HeaderComponent],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
+    ]
+})
+export class CoreModule { }
