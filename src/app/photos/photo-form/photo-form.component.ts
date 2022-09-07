ecc0a0cc4443e287a4afc0bdd830955a34823b77
Caitano <josiasmartinscaitano@gmail.com>
2022-09-06 21:09:25 -0300

    feat: obtendo os dados do formulário

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   public photoForm: FormGroup;
+  public file: File;
 
   constructor(
     private formBuilder: FormBuilder
@@ -20,6 +21,14 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     })
+  }
+
+  public upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
   }
 
 }
